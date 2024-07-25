e1882052afeddaa82b577329a895265c55f8fdcd
andre <andres.hoyos18@eia.edu.co>
2024-07-25 08:10:45 -0500

    Added create-project.sh

---
+++ actividad-01/.scripts/create-project.sh
@@ -0,0 +1,32 @@
+﻿#!/usr/bin/env bash
+
+# Check if the required arguments are passed
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <project_folder> <service_folder>"
+    exit 1
+fi
+
+# project folder, Relative to .scripts location
+PROJECT_FOLDER="$1"
+# Folder in /var/ to store the project
+SERVICE_FOLDER="$2"
+
+# Check if the script is being run as root
+if [ "$EUID" -ne 0 ]; then
+    echo "This script must be run as root. Re-running with sudo..."
+    sudo "$0" "$@"
+    exit $?
+fi
+
+# Move to folder
+cd "$PROJECT_FOLDER" || (echo "Failed to move to $SERVICE_FOLDER" & exit)
+
+# build project
+dotnet restore
+# publish project
+dotnet publish
+
+# create service folder in /var/
+mkdir /var/"$SERVICE_FOLDER"
+# copy publish to service folder
+cp bin/Release/net8.0/publish/ /var/"$SERVICE_FOLDER"
---
+++ actividad-01/.scripts/initialize-instance.sh
@@ -0,0 +1,31 @@
+﻿#!/usr/bin/env bash
+
+## Installs all dependencies, like .git 
+## To run the file you need to make it executable. chmod +x initialize-project.sh
+
+# Function to check if a commmand exists
+command_exists() {
+  command -v "$1" > /dev/null 2>&1
+}
+
+# Check if the script is being run as root
+if [ "$EUID" -ne 0 ]; then
+    echo "This script must be run as root. Re-running with sudo..."
+    sudo "$0" "$@"
+    exit $?
+fi
+
+# Check if .NET is installed
+if ! command_exists dotnet; then
+
+  # install .net from ubuntu
+  echo "Installing .NET from Ubuntu Feed"
+  apt-get update && \
+  apt-get install -y dotnet-sdk-8.0
+fi
+
+# Display the installed .NET SDK version
+echo ".NET SDK version:"
+dotnet --version
+
+echo "Initialization complete"
