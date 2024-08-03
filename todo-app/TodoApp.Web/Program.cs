26d9e7645ba5e837b8be82b32b7ee392cbc62272
Andres Hoyos <andres_hoyos@epam.com>
2024-08-03 01:18:09 -0500

    Configure scripts usage

--- todo-app/TodoApp.Web/Program.cs
+++ todo-app/TodoApp.Web/Program.cs
@@ -4,6 +4,8 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
 app.UseStaticFiles();
+// add this to enable route to static files for webassembly that normally are located on "/". Basically it allows delivering static files located in "/wasm"
+app.UseStaticFiles("/wasm");
 
 // add blazor wasm files to project, under the /wasm path, this is set in the client project <StaticWebAssetBasePath>
 app.UseBlazorFrameworkFiles("/wasm");
