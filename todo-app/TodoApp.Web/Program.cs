32d4897a50a0eaec198d7ff948d0d39b033feb87
Andres Hoyos <andres_hoyos@epam.com>
2024-08-04 20:00:43 -0500

    Add optional compilation for hosting wasm in aspnet. Added appsettings configuration

--- todo-app/TodoApp.Web/Program.cs
+++ todo-app/TodoApp.Web/Program.cs
@@ -4,11 +4,13 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
 app.UseStaticFiles();
+
+#if WASM_HOST
 // add this to enable route to static files for webassembly that normally are located on "/". Basically it allows delivering static files located in "/wasm"
 app.UseStaticFiles("/wasm");
-
 // add blazor wasm files to project, under the /wasm path, this is set in the client project <StaticWebAssetBasePath>
 app.UseBlazorFrameworkFiles("/wasm");
 app.MapFallbackToFile("/wasm/{*path:nonfile}", "wasm/index.html");
+#endif
 
 app.Run();
